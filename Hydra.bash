bd3e0a8bca57424e069e8f3a5e4ad0df971fa998
Gage Greenhouse <gsggage@yahoo.com>
2017-10-25 00:38:19 -0500

    Fixed the Weird Al issue

--- Hydra.bash
+++ Hydra.bash
@@ -7,25 +7,25 @@ if [ ! -d "$2" ]; then
 fi
 
 if [ $4 -eq 1 ]; then
-	iter[1]=[A-Za-z0-9]
+	iter[1]=[A-Za-z0-9']
 elif [ $4 -eq 2 ]; then
 	iter[1]=[0-9A-Ia-i]
-	iter[2]=[J-Zj-z]
+	iter[2]=[J-Zj-z']
 elif [ $4 -eq 3 ]; then
 	iter[1]=[0-9A-Ca-c]
 	iter[2]=[D-Od-o]
-	iter[3]=[P-Zp-z]
+	iter[3]=[P-Zp-z']
 elif [ $4 -eq 4 ]; then
 	iter[1]=[0-9]
 	iter[2]=[A-Ia-i]
 	iter[3]=[J-Rj-r]
-	iter[4]=[S-Zs-z]
+	iter[4]=[S-Zs-z']
 elif [ $4 -eq 5 ]; then
 	iter[1]=[0-7]
 	iter[2]=[8-9A-Ea-e]
 	iter[3]=[F-Lf-l]
 	iter[4]=[M-Sm-s]
-	iter[5]=[T-Zt-z]
+	iter[5]=[T-Zt-z']
 elif [ $4 -eq 8 ]; then
 	iter[1]=[A-Da-d]
 	iter[2]=[E-He-h]
@@ -34,7 +34,7 @@ elif [ $4 -eq 8 ]; then
 	iter[5]=[P-Rp-r]
 	iter[6]=[S-Us-u]
 	iter[7]=[W-Xw-x]
-	iter[8]=[Z0-9]
+	iter[8]=[Z0-9']
 elif [ $4 -eq 12 ]; then
 	iter[1]=[A-Ba-b]
 	iter[2]=[C-Dc-d]
@@ -47,7 +47,7 @@ elif [ $4 -eq 12 ]; then
 	iter[9]=[Q-Rq-r]
 	iter[10]=[S-Ts-t]
 	iter[11]=[U-Vu-v]
-	iter[12]=[Z0-9]
+	iter[12]=[Z0-9']
 fi
 
 for i in $(seq 1 $4); do
