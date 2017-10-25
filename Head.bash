fa83c7e265c31a705aacaf54bde0a2617071ea5f
Gage Greenhouse <gsggage@yahoo.com>
2017-10-25 00:35:08 -0500

    It Fully works now

--- Head.bash
+++ Head.bash
@@ -2,7 +2,8 @@
 # Worker Script Call Layout: ./Head.bash [Music Directory] [Output Directory] [Bitrate in kbs] [Iteration Area eg. [A..G]]
 #echo $1 $2 $3 $4
 l="$1/$4*/*/*"
-for i in "$1/$4*/*/*"; do
+echo $l
+for i in $1/$4*/*/*; do
 	if [ ! -e "$i" ] ; then
 		exit 0
 	fi
@@ -10,11 +11,11 @@ for i in "$1/$4*/*/*"; do
 	i2=${i%/*.*}
 	i3=${i1%/*.*}
 	i4=${i1%.*}.mp3
-	#echo $i
-	#echo $i1
-	#echo $i2
-	#echo $i3
-	#echo $i4
+	echo $i
+	echo $i1
+	echo $i2
+	echo $i3
+	echo $i4
 	if [ ! -d "$2/$i3" ]; then
 		mkdir -p "$2/$i3"
 	fi
--- Hydra.bash
+++ Hydra.bash
@@ -7,54 +7,56 @@ if [ ! -d "$2" ]; then
 fi
 
 if [ $4 -eq 1 ]; then
-	iter1=[A-Za-z0-9]
+	iter[1]=[A-Za-z0-9]
 elif [ $4 -eq 2 ]; then
-	iter1=[0-9A-Ia-i]
-	iter2=[J-Zj-z]
+	iter[1]=[0-9A-Ia-i]
+	iter[2]=[J-Zj-z]
 elif [ $4 -eq 3 ]; then
-	iter1=[0-9A-Ca-c]
-	iter2=[D-Od-o]
-	iter3=[P-Zp-z]
+	iter[1]=[0-9A-Ca-c]
+	iter[2]=[D-Od-o]
+	iter[3]=[P-Zp-z]
 elif [ $4 -eq 4 ]; then
-	iter1=[0-9]
-	iter2=[A-Ia-i]
-	iter3=[J-Rj-r]
-	iter4=[S-Zs-z]
+	iter[1]=[0-9]
+	iter[2]=[A-Ia-i]
+	iter[3]=[J-Rj-r]
+	iter[4]=[S-Zs-z]
 elif [ $4 -eq 5 ]; then
-	iter1=[0-7]
-	iter2=[8-9A-Ea-e]
-	iter3=[F-Lf-l]
-	iter4=[M-Sm-s]
-	iter5=[T-Zt-z]
+	iter[1]=[0-7]
+	iter[2]=[8-9A-Ea-e]
+	iter[3]=[F-Lf-l]
+	iter[4]=[M-Sm-s]
+	iter[5]=[T-Zt-z]
 elif [ $4 -eq 8 ]; then
-	iter1=[A-Da-d]
-	iter2=[E-He-h]
-	iter3=[I-Li-l]
-	iter4=[M-Om-o]
-	iter5=[P-Rp-r]
-	iter6=[S-Us-u]
-	iter7=[W-Xw-x]
-	iter8=[Z0-9]
+	iter[1]=[A-Da-d]
+	iter[2]=[E-He-h]
+	iter[3]=[I-Li-l]
+	iter[4]=[M-Om-o]
+	iter[5]=[P-Rp-r]
+	iter[6]=[S-Us-u]
+	iter[7]=[W-Xw-x]
+	iter[8]=[Z0-9]
 elif [ $4 -eq 12 ]; then
-	iter1=[A-Ba-b]
-	iter2=[C-Dc-d]
-	iter3=[E-Fe-f]
-	iter4=[G-Hg-h]
-	iter5=[I-Ji-j]
-	iter6=[K-Lk-l]
-	iter7=[M-Nm-n]
-	iter8=[L-Pl-p]
-	iter9=[Q-Rq-r]
-	iter10=[S-Ts-t]
-	iter11=[U-Vu-v]
-	iter12=[Z0-9]
+	iter[1]=[A-Ba-b]
+	iter[2]=[C-Dc-d]
+	iter[3]=[E-Fe-f]
+	iter[4]=[G-Hg-h]
+	iter[5]=[I-Ji-j]
+	iter[6]=[K-Lk-l]
+	iter[7]=[M-Nm-n]
+	iter[8]=[L-Pl-p]
+	iter[9]=[Q-Rq-r]
+	iter[10]=[S-Ts-t]
+	iter[11]=[U-Vu-v]
+	iter[12]=[Z0-9]
 fi
 
 for i in $(seq 1 $4); do
-	temp=iter$i
-	#echo ${!temp}
-	"${DIR}/Head.bash" "$1" "$2" "$3" "${!temp}" &
+#	echo $i
+#	temp=${iter[i]}
+#	echo $temp
+	bash "${DIR}/Head.bash" "$1" "$2" "$3" "${temp}" &
 done
+wait
 j=$(jobs -p)
 trap 'kill $j' SIGINT
 wait
