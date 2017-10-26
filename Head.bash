1fd647be510df16e1c9de661e3f6858e62c97734
Gage Greenhouse <gsggage@yahoo.com>
2017-10-26 14:22:58 -0500

    Final Version, I really hope it works this time.

--- Head.bash
+++ Head.bash
@@ -1,21 +1,23 @@
 #!/bin/bash
 # Worker Script Call Layout: ./Head.bash [Music Directory] [Output Directory] [Bitrate in kbs] [Iteration Area eg. [A..G]]
 #echo $1 $2 $3 $4
+
 l="$1/$4*/*/*"
 #echo $l
-for i in $1/$4*/*/*; do
-	if [ ! -e "$i" ] ; then
-		exit 0
-	fi
+nun="$(cat $4)"
+IFS=$'\n'
+#echo "$nun"
+for i in $nun; do
 	i1=${i#$1/}
 	i2=${i%/*.*}
 	i3=${i1%/*.*}
 	i4=${i1%.*}.mp3
-	echo $i
-	echo $i1
-	echo $i2
-	echo $i3
-	echo $i4
+#	echo "$i"
+#	echo "$i1"
+#	echo "$i2"
+#	echo "$i3"
+#	echo "$i4"
+#	echo " "
 	if [ ! -d "$2/$i3" ]; then
 		mkdir -p "$2/$i3"
 	fi
@@ -23,4 +25,3 @@ for i in $1/$4*/*/*; do
 	ffmpeg -i "$i" -ab $3k "$2/$i4"
 	replaygain -r 95 "$2/$i4"
 done
-
--- Hydra.bash
+++ Hydra.bash
@@ -1,62 +1,64 @@
 #!/bin/bash
 # Control Scheme ./Hydra.bash [Music Directory] [Output Directory] [Bitrate in kbs] [Number of Workers 1-35]
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-nun=""
+nun="$(find "$1" -type f -regex .+\.m.+ -printf '%p\n')"
+echo "$nun" >> "$DIR/toEncode.d"
+split "$DIR/toEncode.d" -n l/$4
 if [ ! -d "$2" ]; then
 	mkdir "$2"
 fi
 
 if [ $4 -eq 1 ]; then
-	iter[1]=[A-Za-z0-9']
+	iter[1]=xaa
 elif [ $4 -eq 2 ]; then
-	iter[1]=[0-9A-Ia-i]
-	iter[2]=[J-Zj-z']
+	iter[1]=xaa
+	iter[2]=xab
 elif [ $4 -eq 3 ]; then
-	iter[1]=[0-9A-Ca-c]
-	iter[2]=[D-Od-o]
-	iter[3]=[P-Zp-z']
+	iter[1]=xaa
+	iter[2]=xab
+	iter[3]=xac
 elif [ $4 -eq 4 ]; then
-	iter[1]=[0-9]
-	iter[2]=[A-Ia-i]
-	iter[3]=[J-Rj-r]
-	iter[4]=[S-Zs-z']
+	iter[1]=xaa
+	iter[2]=xab
+	iter[3]=xac
+	iter[4]=xad
 elif [ $4 -eq 5 ]; then
-	iter[1]=[0-7]
-	iter[2]=[8-9A-Ea-e]
-	iter[3]=[F-Lf-l]
-	iter[4]=[M-Sm-s]
-	iter[5]=[T-Zt-z']
+	iter[1]=xaa
+	iter[2]=xab
+	iter[3]=xac
+	iter[4]=xad
+	iter[5]=xae
 elif [ $4 -eq 8 ]; then
-	iter[1]=[A-Da-d]
-	iter[2]=[E-He-h]
-	iter[3]=[I-Li-l]
-	iter[4]=[M-Om-o]
-	iter[5]=[P-Rp-r]
-	iter[6]=[S-Us-u]
-	iter[7]=[W-Xw-x]
-	iter[8]=[Z0-9']
+	iter[1]=xaa
+	iter[2]=xab
+	iter[3]=xac
+	iter[4]=xad
+	iter[5]=xae
+	iter[6]=xaf
+	iter[7]=xag
+	iter[8]=xah
 elif [ $4 -eq 12 ]; then
-	iter[1]=[A-Ba-b]
-	iter[2]=[C-Dc-d]
-	iter[3]=[E-Fe-f]
-	iter[4]=[G-Hg-h]
-	iter[5]=[I-Ji-j]
-	iter[6]=[K-Lk-l]
-	iter[7]=[M-Nm-n]
-	iter[8]=[L-Pl-p]
-	iter[9]=[Q-Rq-r]
-	iter[10]=[S-Ts-t]
-	iter[11]=[U-Vu-v]
-	iter[12]=[Z0-9']
+	iter[1]=xaa
+	iter[2]=xab
+	iter[3]=xac
+	iter[4]=xad
+	iter[5]=xae
+	iter[6]=xaf
+	iter[7]=xag
+	iter[8]=xah
+	iter[9]=xai
+	iter[10]=xaj
+	iter[11]=xak
+	iter[12]=xal
 fi
 
 for i in $(seq 1 $4); do
 #	echo $i
-#	temp=${iter[i]}
-#	echo $temp
-	bash "${DIR}/Head.bash" "$1" "$2" "$3" "${temp}" &
+	temp=${iter[i]}
+	echo $temp
+	./Head.bash "$1" "$2" "$3" "${temp}" &
 done
-wait
 j=$(jobs -p)
 trap 'kill $j' SIGINT
 wait
+rm toEncode.d xa*
