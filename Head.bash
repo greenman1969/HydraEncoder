faa39a0cc962957668a806e4441f962c12873bae
greenman1969 <gsggage@yahoo.com>
2018-05-11 17:10:59 -0500

    Changed Log Level of FFmpeg to make it quieter

---
+++ Head.bash
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Worker Script Call Layout: ./Head.bash [Music Directory: Omit trailing /] [Output Directory: omit trailing /] [Bitrate in kbs] [File listing files]
+#echo $1 $2 $3 $4
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+nun="$(cat $DIR/$4)"
+IFS=$'\n'
+#echo "$nun"
+for i in $nun; do
+	i1=${i#$1/}
+	i2=${i%/*.*}
+	i3=${i1%/*.*}
+	i4=${i1%.*}.mp3
+#	echo "$i"
+#	echo "$i1"
+#	echo "$i2"
+#	echo "$i3"
+#	echo "$i4"
+#	echo " "
+	if [ ! -d "$2/$i3" ]; then
+		mkdir -p "$2/$i3"
+	fi
+#	echo "../$1/${i%.*}.mp3"
+	ffmpeg -hide_banner -loglevel warning -n -i "$i" -ab $3k "$2/$i4"
+	replaygain -r 95 "$2/$i4"
+done
---
+++ Hydra.bash
@@ -0,0 +1,132 @@
+#!/bin/bash
+# Control Scheme ./Hydra.bash [Music Directory: Omit Trailing /] [Output Directory: Omit Trailing /] [Bitrate in kbs] [Number of Workers: 1-8,12]
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+nun="$(find "$1" -type f -regex .+\.m.+ -printf '%p\n')"
+echo "$nun" >> "$DIR/toEncode.d"
+$(cd "$DIR" && split "$DIR/toEncode.d" -n l/$4)
+if [ ! -d "$2" ]; then
+	mkdir "$2"
+fi
+
+if [ $4 -eq 1 ]; then
+	iter[1]=xaa
+elif [ $4 -eq 2 ]; then
+	iter[1]=xaa
+	iter[2]=xab
+elif [ $4 -eq 3 ]; then
+	iter[1]=xaa
+	iter[2]=xab
+	iter[3]=xac
+elif [ $4 -eq 4 ]; then
+	iter[1]=xaa
+	iter[2]=xab
+	iter[3]=xac
+	iter[4]=xad
+elif [ $4 -eq 5 ]; then
+	iter[1]=xaa
+	iter[2]=xab
+	iter[3]=xac
+	iter[4]=xad
+	iter[5]=xae
+elif [ $4 -eq 6 ]; then
+        iter[1]=xaa
+        iter[2]=xab
+        iter[3]=xac
+        iter[4]=xad
+        iter[5]=xae
+        iter[6]=xaf
+elif [ $4 -eq 7 ]; then
+        iter[1]=xaa
+        iter[2]=xab
+        iter[3]=xac
+        iter[4]=xad
+        iter[5]=xae
+        iter[6]=xaf
+        iter[7]=xag
+elif [ $4 -eq 8 ]; then
+	iter[1]=xaa
+	iter[2]=xab
+	iter[3]=xac
+	iter[4]=xad
+	iter[5]=xae
+	iter[6]=xaf
+	iter[7]=xag
+	iter[8]=xah
+elif [ $4 -eq 10 ]; then
+        iter[1]=xaa
+        iter[2]=xab
+        iter[3]=xac
+        iter[4]=xad
+        iter[5]=xae
+        iter[6]=xaf
+        iter[7]=xag
+        iter[8]=xah
+        iter[9]=xai
+        iter[10]=xaj
+elif [ $4 -eq 11 ]; then
+        iter[1]=xaa
+        iter[2]=xab
+        iter[3]=xac
+        iter[4]=xad
+        iter[5]=xae
+        iter[6]=xaf
+        iter[7]=xag
+        iter[8]=xah
+        iter[9]=xai
+        iter[10]=xaj
+        iter[11]=xak
+elif [ $4 -eq 12 ]; then
+	iter[1]=xaa
+	iter[2]=xab
+	iter[3]=xac
+	iter[4]=xad
+	iter[5]=xae
+	iter[6]=xaf
+	iter[7]=xag
+	iter[8]=xah
+	iter[9]=xai
+	iter[10]=xaj
+	iter[11]=xak
+	iter[12]=xal
+elif [ $4 -eq 24 ]; then
+	iter[1]=xaa
+	iter[2]=xab
+	iter[3]=xac
+	iter[4]=xad
+	iter[5]=xae
+	iter[6]=xaf
+	iter[7]=xag
+	iter[8]=xah
+	iter[9]=xai
+	iter[10]=xaj
+	iter[11]=xak
+	iter[12]=xal
+	iter[13]=xam
+	iter[14]=xan
+	iter[15]=xao
+	iter[16]=xap
+	iter[17]=xaq
+	iter[18]=xar
+	iter[19]=xas
+	iter[20]=xat
+	iter[21]=xau
+	iter[22]=xav
+	iter[23]=xaw
+	iter[24]=xax
+else
+	echo "Please enter a valid number of workers"
+	rm $DIR/toEncode.d
+	rm $DIR/x*
+	exit
+fi
+
+for i in $(seq 1 $4); do
+#	echo $i
+	temp=${iter[i]}
+	echo $temp
+	$DIR/Head.bash "$1" "$2" "$3" "${temp}" &
+done
+j=$(jobs -p)
+trap 'kill $j' SIGINT
+wait
+rm $DIR/toEncode.d $DIR/xa*
